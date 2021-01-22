720c56c6b335203d095547f67d2a98a375d47a53
wanerge <wanerge100@hotmail.com>
2021-01-22 13:18:12 -0500

    se agregaron las funciones de muerte del personaje

--- life.cpp
+++ life.cpp
@@ -1,12 +1,13 @@
 #include "life.h"
 
-life::life(QMainWindow *s, int num_vidas, int max_x_view)
+life::life(QMainWindow *s, int num_vidas, int max_x_view, int pos_x, int pos_y)
 {
     view = new QGraphicsView(s);
     escena = new QGraphicsScene;
     vidas = new QVector<QGraphicsPixmapItem *>;
 
-    view->resize(max_x_view,32);
+    view->setGeometry(pos_x,pos_y,max_x_view,32);
+    //view->resize(max_x_view, 32);
     view->setScene(escena);
     view->show();
     view->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
@@ -26,6 +27,11 @@ void life::eliminar_vida()
 {
     escena->removeItem(vidas->last());
     vidas->removeLast();
+}
+
+QVector<QGraphicsPixmapItem *> *life::getVidas() const
+{
+    return vidas;
 }
 
 life::~life()
